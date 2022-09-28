88823129b5e30a75a16caf07680f359232cf6152
kimgeunhoe <ghkim1031@naver.com>
2022-09-28 13:13:24 +0900

    Create DataBaseBuilder.java

---
+++ 06_final_jsp_fileUpload/src/main/java/orn/DataBaseBuilder.java
@@ -0,0 +1,28 @@
+package orn;
+
+import java.io.IOException;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataBaseBuilder {
+	private static Logger log = LoggerFactory.getLogger(DataBaseBuilder.class);
+	private static SqlSessionFactory factory;
+	
+	static {
+		try {
+			factory = new SqlSessionFactoryBuilder()
+					.build(Resources.getResourceAsReader("orn/MybatisConfig.xml"));
+		} catch (IOException e) {
+			log.info(">>>> Mybatis Configuration Error");
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSessionFactory getFactory() {
+		return factory;
+	}
+}
